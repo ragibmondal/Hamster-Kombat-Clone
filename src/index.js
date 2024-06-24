f606e49d9fa7902ac66641fc25ad653369b62019
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2024-06-24 12:13:45 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import Home from './components/Home';
+import Referral from './components/Referral';
+import Tasks from './components/Tasks';
+import Boost from './components/Boost';
+import Stats from './components/Stats';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'referral', element: <Referral /> },
+      { path: 'tasks', element: <Tasks /> },
+      { path: 'boost', element: <Boost /> },
+      { path: 'stats', element: <Stats /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
