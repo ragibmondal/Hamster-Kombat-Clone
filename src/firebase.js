c5fe00b4ad5a3ba543cfe1a02eb7d69257d688b0
T7890u <161718235+T7890u@users.noreply.github.com>
2024-06-27 13:34:40 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // Replace with your Firebase configuration
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+  apiKey: "AIzaSyCcVnfwboER0CnB35alqFq0UtoVmYCUhh4",
+  authDomain: "mytonix-a8dc6.firebaseapp.com",
+  projectId: "mytonix-a8dc6",
+  storageBucket: "mytonix-a8dc6.appspot.com",
+  messagingSenderId: "944367304454",
+  appId: "1:944367304454:web:b336e8fbbfdd749974a887",
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
